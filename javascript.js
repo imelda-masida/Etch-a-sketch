416d67fc5fde7ed0b61a284a43eba0fbbc4de31b
stone <imeldamasidakakudji@gmail.com>
2026-07-19 22:40:09 +0200

    fonction pour la ggestion des nombres entier

--- javascript.js
+++ javascript.js
@@ -3,3 +3,7 @@ const bouton = document.createElement("button");
 
 bouton.innerText = "Nouvelle grille";
 document.body.insertBefore(bouton, conteneur);
+
+function nbentierAleatoire(max) {
+    return Math.floor(Math.random() * max);
+}
